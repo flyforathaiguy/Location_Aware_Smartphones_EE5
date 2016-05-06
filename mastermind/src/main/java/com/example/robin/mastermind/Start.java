899ca74136d7517d5914504f100a02ead6233e57
flyforathaiguy <robin0croon@gmail.com>
2016-05-06 15:54:00 +0200

    start class for game mastermind

--- mastermind/src/main/java/com/example/robin/mastermind/Start.java
+++ mastermind/src/main/java/com/example/robin/mastermind/Start.java
@@ -1,7 +1,89 @@
 package com.example.robin.mastermind;
 
-/**
- * Created by Robin on 28/04/2016.
- */
-public class Start {
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Build;
+import android.os. Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import be.groept.emedialab.util.GlobalResources;
+
+public class Start extends AppCompatActivity{
+private View mContentView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_setup);
+        GlobalResources.getInstance().getDevice().setMac(BluetoothAdapter.getDefaultAdapter().getAddress());
+        mContentView = findViewById(R.id.coverLayout);
+        Button createButton = (Button) findViewById(R.id.create);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), ConnectionActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putBoolean("create", true);
+                //bundle.putBoolean("join", false);
+                //bundle.putBoolean("rules", false);
+                intent.putExtras(bundle);
+                Start.this.startActivity(intent);
+            }
+        });
+
+        Button joinButton = (Button) findViewById(R.id.join);
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), ConnectionActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putBoolean("create", false);
+                bundle.putBoolean("rules", false);
+                intent.putExtras(bundle);
+                Start.this.startActivity(intent);
+            }
+        });
+        Button rulesButton = (Button) findViewById(R.id.rules);
+        rulesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), ConnectionActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putBoolean("create", false);
+                bundle.putBoolean("join", false);
+                intent.putExtras(bundle);
+                Start.this.startActivity(intent);
+            }
+        });
+
+        hide();
+    }
+
+    @SuppressLint("InlinedApi")
+    private void hide(){
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null)
+            actionBar.hide();
+
+        //Lollipop and higher
+        if(Build.VERSION.SDK_INT >= 21){
+            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        hide();
+    }
 }
