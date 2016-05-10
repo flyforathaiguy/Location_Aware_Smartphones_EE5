8ac1c979c8e9d871379bac60644af2386ea3a8b8
flyforathaiguy <robin0croon@gmail.com>
2016-05-10 14:13:06 +0200

    Connection activity fixed for the pattern search game

--- pattern_search/src/main/java/com/example/robin/pattern_search/ConnectionActivity.java
+++ pattern_search/src/main/java/com/example/robin/pattern_search/ConnectionActivity.java
@@ -1,39 +1,79 @@
 package com.example.robin.pattern_search;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+
+import be.groept.emedialab.fragments.ClientFragment;
+import be.groept.emedialab.fragments.PartyReadyListener;
+import be.groept.emedialab.fragments.ServerFragment;
 
 
-public class ConnectionActivity extends ActionBarActivity {
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class ConnectionActivity extends FragmentActivity implements PartyReadyListener {
+
+    private static final String TAG = "SecondActivity";
+
+    public static final int minNumberOfDevices = 1;
+    public static final int maxNumberOfDevices = 6;
+
+    private View mContentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
-    }
+        mContentView = findViewById(R.id.fullscreen_content);
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_connection, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        Bundle bundle = getIntent().getExtras();
+        if(bundle.getBoolean("create")){
+            Bundle bundle2 = new Bundle();
+            bundle2.putInt("minNumberOfDevices", minNumberOfDevices);
+            bundle2.putInt("maxNumberOfDevices", maxNumberOfDevices);
+            bundle2.putString("Theme", "HMT");
+            ServerFragment serverFragment = new ServerFragment();
+            serverFragment.setArguments(bundle2);
+            fragmentTransaction.add(R.id.fragment_container, serverFragment);
+        }else{
+            Bundle b = new Bundle();
+            b.putString("Theme", "HMT");
+            ClientFragment client = new ClientFragment();
+            client.setArguments(b);
+            fragmentTransaction.add(R.id.fragment_container, client);
         }
+        fragmentTransaction.commit();
 
-        return super.onOptionsItemSelected(item);
+        hide();
+    }
+
+    private void hide(){
+        if(Build.VERSION.SDK_INT >= 21){
+            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hide();
+    }
+
+    @Override
+    public void partyReady() {
+        Log.d(TAG, "The party is ready, dear sir.");
+        startActivity(new Intent(this, GameWindow.class));
     }
 }
