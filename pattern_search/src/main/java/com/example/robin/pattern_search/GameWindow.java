738200b76968581c83f50150b6f07b0969f9f2c6
flyforathaiguy <robin0croon@gmail.com>
2016-05-12 11:08:18 +0200

    Update GameWindow class

--- pattern_search/src/main/java/com/example/robin/pattern_search/GameWindow.java
+++ pattern_search/src/main/java/com/example/robin/pattern_search/GameWindow.java
@@ -1,22 +1,20 @@
 package com.example.robin.pattern_search;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
@@ -24,6 +22,7 @@ import be.groept.emedialab.communications.DataHandler;
 import be.groept.emedialab.communications.DataPacket;
 import be.groept.emedialab.image_manipulation.PatternDetector;
 import be.groept.emedialab.image_manipulation.RunPatternDetector;
+import be.groept.emedialab.server.data.Device;
 import be.groept.emedialab.server.data.Position;
 import be.groept.emedialab.util.GlobalResources;
 
@@ -32,20 +31,18 @@ public class GameWindow extends Activity {
 
     private Button button;
     private TextView feedbackText;
+    private TextView ratingFeedback;
+    private TextView positionText;
+    private RatingBar rating;
+    private double randomX;
+    private double randomY;
+    private boolean wonGame;
+    private double stars;
 
     private static final int END_GAME = 3;
     private static final int POS_CONFIRM = 2;
+    private static final int LAUNCH_WIN = 1;
 
-    //Bounds wherein the position can be randomly generated
-    private static final int LOWER_BOUND_X = -90;
-    private static final int UPPER_BOUND_X = 90;
-    private static final int LOWER_BOUND_Y = -90;
-    private static final int UPPER_BOUND_Y = 90;
-
-    private List<DevicePositionPair> confirmedPairs;
-    private List<DevicePositionPair> wantedPairs;
-
-    private final int nbDevices = GlobalResources.getInstance().getDevices().size() + 1;
 
     final Activity activity = this;
 
@@ -53,6 +50,23 @@ public class GameWindow extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_window);
+
+        //Initialize the rating and the feedback
+        rating = (RatingBar) findViewById(R.id.rating);
+        ratingFeedback = (TextView) findViewById(R.id.ratingFeedback);
+        rating.setRating(0);
+
+        //Initialize the position text
+        positionText = (TextView) findViewById(R.id.positionText);
+
+
+        //If you're the master, you have to make a random location
+        //of where the players need to find you
+        if(GlobalResources.getInstance().getClient() == false) {
+            makeRandomLocation();
+        }
+
+
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         GlobalResources.getInstance().setHandler(handler);
 
@@ -68,11 +82,6 @@ public class GameWindow extends Activity {
         };
         runnablePattern.run();
 
-        //Create the list
-        confirmedPairs = new ArrayList<>();
-
-        //Calculate the game
-        calculateGame();
 
         //Get accept button
         button = (Button) findViewById(R.id.confirmButton);
@@ -84,9 +93,6 @@ public class GameWindow extends Activity {
 
             ;
         });
-
-        //Get feedback text
-        feedbackText = (TextView) findViewById(R.id.feedbackText);
     }
 
     //Handler for signals, large parts of code taken from original Location Aware Smartphones project
@@ -96,102 +102,110 @@ public class GameWindow extends Activity {
             super.handleMessage(msg);
             //Log.d(TAG, "Handler called");
             //Data packet incoming
-            if(msg.what == DataHandler.DATA_TYPE_COORDINATES) {
-                //If coordinates are received, the master has to handle these coordinates
-                //and store them in a list
-
-            }else if(msg.what == DataHandler.DATA_TYPE_DEVICE_DISCONNECTED){
-                // TODO: Wat als device disconnect?
+            if(msg.what == DataHandler.DATA_TYPE_DEVICE_DISCONNECTED){
                 //Don't know yet, probalby end the game
                 endGame();
             }
             else if(msg.what == DataHandler.DATA_TYPE_OWN_POS_UPDATED){
-                //updateText();
-            }
-            else if(msg.what == DataHandler.DATA_TYPE_DATA_PACKET) {
-                //Datapacket = confirmation of the position of a phone
-                //When one of the phones sends his confirmation, this means that this position for this phone is final
-                //So it can be put in a list, to check later on
-                Serializable data;
-                if ((data = GlobalResources.getInstance().readData()) != null) {
-                    handleData((DataPacket) data);
-                }
+                updatePosition();
             }
         }
     };
 
+    private void updatePosition(){
+
+        Position position = GlobalResources.getInstance().getDevice().getPosition();
+        if(position.getFoundPattern()){
+            positionText.setTextColor(Color.parseColor("blue"));
+            positionText.setText(String.format("%s (%.2f, %.2f, %.2f) %.1f°", getText(be.groept.emedialab.R.string.CalibrateOwnPosition), position.getX(), position.getY(), position.getZ(), position.getRotation()));
+
+        }else{
+            positionText.setTextColor(Color.parseColor("red"));
+        }
+    }
+
     private void handleData(DataPacket dataPacket){
         //Always read in the string from the sender of the data, to maintain data usage
         switch(dataPacket.getDataType()){
-            case POS_CONFIRM:
-                //Make new DevicePositionPair containing the address of the device from which the position was sent, as well as the position
-                //Get the device address from the phone that confirmed its position
-                String device = GlobalResources.getInstance().getReceivedList().get(GlobalResources.getInstance().getReceivedList().size() - 1);
-                DevicePositionPair pair = new DevicePositionPair(device, GlobalResources.getInstance().getDevices().get(device));
-                confirmedPairs.add(pair);
-                break;
-            //case LAUNCH_WIN:
-                //launchWinIntent();
+            case LAUNCH_WIN:
+                launchWinIntent();
             case END_GAME:
                 endGame();
             default:
                 break;
         }
-        //Remove address at last index since we do not need it
-        //Should be the only one in the list ( list.size() == 1 --> index 0)
-        //Log.d(TAG, "received list size: " + GlobalResources.getInstance().getReceivedList().size());
-        if(GlobalResources.getInstance().getReceivedList().size() > 0)
-            GlobalResources.getInstance().getReceivedList().remove(GlobalResources.getInstance().getReceivedList().size() - 1);
     }
 
-    private void calculateGame(){
-
-        //Get all devices, current position of them does not matter
-        HashMap<String, Position> connectedDevices = (HashMap) GlobalResources.getInstance().getDevices();
-        List<String> devicesList = new ArrayList<>(connectedDevices.keySet());
-        //Add own device to the list
-        if(connectedDevices.containsKey("ownpos") == false)
-            devicesList.add("ownpos");
-
-        //Populate the wantedPairs list
-        Random random = new Random();
-        for(int i = 0; i < devicesList.size(); i++) {
-            //Generate random int between upper bound and lower bound, both inclusive
-            Position wantedPosition;
-            int randomX = (random.nextInt(UPPER_BOUND_X - LOWER_BOUND_X) + UPPER_BOUND_X);
-            int randomY = (random.nextInt(UPPER_BOUND_Y - LOWER_BOUND_Y) + UPPER_BOUND_Y);
-
-            //Z value and angle at 0°, do not actually matter
-            wantedPosition = new Position(randomX, randomY, 0, 0);
-            //Create new DevicePositionPair and add it to the wantedPairs list
-            DevicePositionPair pair = new DevicePositionPair(devicesList.get(i), wantedPosition);
-            wantedPairs.add(pair);
-        }
-
-        //TODO: What else has to happen?
+    private void launchWinIntent() {
+        wonGame = true;
+        Intent intent = new Intent(getBaseContext(), WinnerActivity.class);
+        startActivity(intent);
     }
 
     private void confirmButton() {
 
         button.setClickable(false);
 
-        //In this case you're the client
-        if(GlobalResources.getInstance().getClient() == true) {
-            //Send data of type DATA_PACKET, DataPacket itself is of type COLOR
-            Position position = GlobalResources.getInstance().getDevice().getPosition();
-            GlobalResources.getInstance().sendData(DataHandler.DATA_TYPE_DATA_PACKET, new DataPacket(POS_CONFIRM));
-        }
-
-        //In this case you're the master
-        else {
-            //TODO: calculate the distance for each phone to the wanted position
-            checkAllDevicesConfirmed();
-        }
-
+        //Calculate how far the target is from each phone
+        targetCalculation();
     }
 
-    private void checkAllDevicesConfirmed(){
+    private void targetCalculation() {
+        //Iterates through all of the devices in the map
+        //and calculates the distance between the random
+        //location and the position of the phone
+        for(Map.Entry<String, Position> entry : GlobalResources.getInstance().getDevices().entrySet()){
+            if(entry.getKey().equals(GlobalResources.getInstance().getDevice())) {
+                Location loc1 = new Location("");
+                loc1.setLatitude(entry.getValue().getX());
+                loc1.setLongitude(entry.getValue().getY());
 
+                Location loc2 = new Location("");
+                loc2.setLatitude(randomX);
+                loc2.setLongitude(randomY);
+
+                float distanceInMeters = loc1.distanceTo(loc2);
+
+                if(distanceInMeters <= 4) {
+
+                    launchWinIntent();
+
+                }
+
+                ratingCalculation(GlobalResources.getInstance().getDevice(), distanceInMeters);
+            }
+        }
+    }
+
+    private void ratingCalculation(Device device, float distance) {
+
+        if(distance <= 8) {
+            rating.setRating(4);
+            ratingFeedback.setText("Getting real close!");
+        }
+
+        else if(distance <= 20) {
+            rating.setRating(3);
+            ratingFeedback.setText("Almost there!");
+
+        }
+
+        else if(distance <= 40) {
+            rating.setRating(2);
+            ratingFeedback.setText("On the way!");
+
+        }
+
+        else if(distance <= 70) {
+            rating.setRating(1);
+            ratingFeedback.setText("Better than nothing!");
+
+        }
+
+        else {
+            rating.setRating(0);
+            ratingFeedback.setText("Nowhere near!");
+        }
     }
 
     private void endGame(){
@@ -214,4 +228,82 @@ public class GameWindow extends Activity {
         finish();
     }
 
+
+    private void makeRandomLocation(){
+
+        //Getting the position of the master
+        Position masterPosition = GlobalResources.getInstance().getDevice().getPosition();
+        double masterX = masterPosition.getX();
+        double masterY = masterPosition.getY();
+
+        //Boundaries of where the phone can detect the pattern are hardcoded
+        //Left to right boundary is 90cm
+        //Top to bottom is 130cm
+        Random rand = new Random();
+        int randomNumberX = (rand.nextInt(90)-45);
+        int randomNumberY = (rand.nextInt(130)-65);
+
+        //Check if the random location is not too close to the master phone,
+        //otherwise the game will end too soon
+        while(Math.abs(masterX - randomNumberX) <= 12) {
+
+            randomNumberX = (rand.nextInt(90)-45);
+            randomX = randomNumberX;
+
+        }
+
+        while(Math.abs(masterY - randomNumberY) <= 20) {
+
+            randomNumberY = (rand.nextInt(130)-65);
+            randomY = randomNumberY;
+        }
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        //If this Activity is paused due to the Calibration being launched, do not destroy pattern detector!
+        if(GlobalResources.getInstance().getPatternDetector() != null) {
+            if(GlobalResources.getInstance().getCalibrated() == true) {
+                if(wonGame == false) {
+                    GlobalResources.getInstance().getPatternDetector().destroy();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(GlobalResources.getInstance().getPatternDetector() != null && GlobalResources.getInstance().getPatternDetector().isPaused())
+            GlobalResources.getInstance().getPatternDetector().setup();
+
+        //Coming back from the feedback screen / won game screen?
+        if(wonGame == true){
+            //Re-enable all buttons
+            button.setClickable(true);
+
+            if(wonGame == true){
+                wonGame = false;
+            }
+        }
+    }
+
+    private Thread getThread(){
+        return new Thread() {
+            @Override
+            public void run() {
+                new RunPatternDetector(activity);
+            }
+        };
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        PatternDetector patternDetector = GlobalResources.getInstance().getPatternDetector();
+        if(patternDetector != null)
+            patternDetector.destroy();
+    }
 }
