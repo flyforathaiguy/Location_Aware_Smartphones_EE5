331cd7b55aa69e4471dc8c538bc55ce2546fc0d6
flyforathaiguy <robin0croon@gmail.com>
2016-05-12 17:52:58 +0200

    start class for pattern search

--- pattern_search/src/main/java/com/example/robin/pattern_search/StartActivity.java
+++ pattern_search/src/main/java/com/example/robin/pattern_search/StartActivity.java
@@ -6,15 +6,25 @@ import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-
 import android.view.View;
+
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+//import android.widget.ImageView;
+import android.widget.TextView;
 
 import be.groept.emedialab.util.GlobalResources;
 
-
-public class StartActivity extends Activity {
+public class StartActivity extends Activity{
     private View mContentView;
+
+    TextView txtMessage;
+
+    //Animation
+  Animation animMove;
+Animation animBounce;
+    Animation animSlide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,55 +32,90 @@ public class StartActivity extends Activity {
         setContentView(R.layout.activity_start);
         GlobalResources.getInstance().getDevice().setMac(BluetoothAdapter.getDefaultAdapter().getAddress());
         mContentView = findViewById(R.id.Pattern_Search);
+        //setContentView(R.layout.activity_start);
+        txtMessage = (TextView) findViewById(R.id.textView);
 
-        Button createButton = (Button) findViewById(R.id.join);
+        // load the animations
+        animMove = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.move);
+
+                txtMessage.setVisibility(View.VISIBLE);
+                // start the animation
+                txtMessage.startAnimation(animMove);
+
+        animBounce = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.bounce);
+        Button JOINButton = (Button) findViewById(R.id.join);
+        JOINButton.setVisibility(View.VISIBLE);
+        JOINButton.startAnimation(animBounce);
+
+        Button createButton = (Button) findViewById(R.id.start_new);
+        createButton.setVisibility(View.VISIBLE);
+        createButton.startAnimation(animBounce);
+
+        animBounce = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.bounce);
+        Button RULESButton = (Button) findViewById(R.id.rules);
+        RULESButton.setVisibility(View.VISIBLE);
+        createButton.startAnimation(animBounce);
+
+
+        JOINButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                           Intent intent = new Intent(getBaseContext(), ConnectionActivity.class);
+                            Bundle bundle = new Bundle();
+                            bundle.putBoolean("JOIN", true);
+                            intent.putExtras(bundle);
+                            StartActivity.this.startActivity(intent);
+
+                        }
+                    });
+
         createButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), ConnectionActivity.class);
-                Bundle bundle = new Bundle();
-                bundle.putBoolean("JOIN", true);
-                intent.putExtras(bundle);
-                StartActivity.this.startActivity(intent);
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(getBaseContext(), ConnectionActivity.class);
+                            Bundle bundle = new Bundle();
+                            bundle.putBoolean("JOIN", false);
+                            intent.putExtras(bundle);
+                            StartActivity.this.startActivity(intent);
+                        }
+                    });
+
+                    RULESButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(getBaseContext(), ConnectionActivity.class);
+                            StartActivity.this.startActivity(intent);
+                        }
+                    });
+
+                    hide();
+                }
+
+                @SuppressLint("InlinedApi")
+                private void hide() {
+
+                    //Lollipop and higher
+                    if (Build.VERSION.SDK_INT >= 21) {
+                        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+                    }
+                }
+
+                @Override
+                protected void onResume() {
+                    super.onResume();
+                    hide();
 
             }
-        });
-        Button joinButton = (Button) findViewById(R.id.start_new);
-        joinButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), ConnectionActivity.class);
-                Bundle bundle = new Bundle();
-                bundle.putBoolean("JOIN", false);
-                intent.putExtras(bundle);
-                StartActivity.this.startActivity(intent);
-            }
-        });
 
-        hide();
-    }
-
-    @SuppressLint("InlinedApi")
-    private void hide() {
-
-        //Lollipop and higher
-        if (Build.VERSION.SDK_INT >= 21) {
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        hide();
-    }
-}
-
-
+         }
 
 
