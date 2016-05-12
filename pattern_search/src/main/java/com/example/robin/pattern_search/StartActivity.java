8808d31ecb08f959c132365c08359a60a8ec8bdb
flyforathaiguy <robin0croon@gmail.com>
2016-05-12 17:08:08 +0200

    Changed startactivity

--- pattern_search/src/main/java/com/example/robin/pattern_search/StartActivity.java
+++ pattern_search/src/main/java/com/example/robin/pattern_search/StartActivity.java
@@ -1,39 +1,75 @@
 package com.example.robin.pattern_search;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Build;
+import android.os. Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import be.groept.emedialab.util.GlobalResources;
 
-public class StartActivity extends ActionBarActivity {
+public class StartActivity extends AppCompatActivity{
+
+    private View mContentView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_start);
+        GlobalResources.getInstance().getDevice().setMac(BluetoothAdapter.getDefaultAdapter().getAddress());
+        mContentView = findViewById(R.id.Pattern_Search);
+        Button createButton = (Button) findViewById(R.id.start_new);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), ConnectionActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putBoolean("create", true);
+                intent.putExtras(bundle);
+                StartActivity.this.startActivity(intent);
+            }
+        });
+
+        Button joinButton = (Button) findViewById(R.id.join);
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), ConnectionActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putBoolean("create", false);
+                intent.putExtras(bundle);
+                StartActivity.this.startActivity(intent);
+            }
+        });
+
+        hide();
     }
 
+    @SuppressLint("InlinedApi")
+    private void hide(){
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null)
+            actionBar.hide();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_start, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        //Lollipop and higher
+        if(Build.VERSION.SDK_INT >= 21){
+            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    protected void onResume(){
+        super.onResume();
+        hide();
     }
 }
